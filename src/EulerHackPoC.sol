a4ff2a30b2c6a1124c75544893ceb641f3d79049
Ana Julia <anajuliabit@gmail.com>
2023-10-28 12:10:21 -0300

    First commit

---
+++ src/EulerHackPoC.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+// TODO update sol version
+pragma solidity ^0.8.13;
+
+contract Main {
+// 1. Flahsloan 30M DAI from AaveV2
+}
+
+contract Violator {
+}
+
+contract Liquidator
